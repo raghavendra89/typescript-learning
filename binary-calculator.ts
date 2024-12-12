854b8da1adb3018f99f68636d63227fc79793b29
Raghavendra <nraghavendra89@gmail.com>
2024-12-12 14:26:07 +0530

    Implement binary subtraction.

--- binary-calculator.ts
+++ binary-calculator.ts
@@ -5,9 +5,13 @@ const _n: (string: string) => number = (number) => {
     return parseInt(number || '0');
 }
 
+const _r: (number: number | string) => string = (number) => {
+    return number.toString().split('').reverse().join('');
+}
+
 const binaryAdd: BinaryFunc = (a, b): number => {
-    a = a.toString().split('').reverse().join('');
-    b = b.toString().split('').reverse().join('');
+    a = _r(a);
+    b = _r(b);
 
     let addedNumber: string = '';
     let carry: number = 0;
@@ -30,7 +34,33 @@ const binaryAdd: BinaryFunc = (a, b): number => {
 }
 
 const binarySubtract: BinaryFunc = (a, b): number => {
-    return -1;
+    let bigNum = a > b ? a : b;
+    let smallNum = a > b ? b : a;
+    let sign: number = a > b ? 1 : -1;
+
+    bigNum = _r(bigNum);
+    smallNum = _r(smallNum);
+
+    let subTractedNumber: string = '';
+    let borrow: number = 0;
+
+    let index = 0;
+    while (index < Math.max(bigNum.length, smallNum.length)) {
+        let diff = _n(bigNum[index]) - _n(smallNum[index]) + borrow;
+        if (diff < 0) {
+            borrow = 1;
+            diff = 1;
+        }
+
+        if (diff == 2) {
+            borrow = diff = 0;
+        }
+
+        subTractedNumber = diff.toString() + subTractedNumber;
+        index++;
+    }
+
+    return _n(subTractedNumber) * sign;
 }
 
 console.log(binaryAdd(10, 11)); // 101
@@ -38,3 +68,5 @@ console.log(binaryAdd(101, 11)); // 1000
 console.log(binaryAdd(111, 111)); // 1110
 console.log(binarySubtract(101, 11)); // 10
 console.log(binarySubtract(11, 110)); // -11
+console.log(binarySubtract(1000, 111)); // 1
+console.log(binarySubtract(111, 1010)); // -11
